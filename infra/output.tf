0b589e4cadb7462758e9db6e88c7f8a6eafc21d8
christophefromparis <christophe.cosnefroy@gmail.com>
2019-04-25 16:10:11 +0200

    Remove weird output

--- infra/output.tf
+++ infra/output.tf
@@ -1,23 +1,15 @@
 output "aws_default_region" {
   value = "${var.aws_default_region}"
 }
-
 output "fqdn_suffix" {
   value = "${var.fqdn_prefix}.${var.aws_route53_name}"
 }
-
 output "k8s_endpoint" {
   value = "${aws_eks_cluster.eks.endpoint}"
 }
-
 output "eks-node-arn" {
   value = "${aws_iam_role.eks-node.arn}"
 }
-
 output "creator_label" {
   value = "${var.creator_label}"
-}
-
-output "client_certificate" {
-  value = "${aws_eks_cluster.eks.}"
 }
